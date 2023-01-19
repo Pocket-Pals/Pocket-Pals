34977f8cb36ac88e96b678a4efac4079db3e4b30
sn00ze <cjavillo@my.bcit.ca>
2023-01-18 23:58:13 -0800

    game boilerplate almost complete

--- src/components/Game/game.jsx
+++ src/components/Game/game.jsx
@@ -1,15 +1,24 @@
 import {useState, useEffect} from 'react' 
 import Phaser from 'phaser'
-// import sky from './sky.png'
+
 
 export default function Game(){
 
-  const [game, setGame] = useState()
+  const [game, setGame] = useState({})
 
   let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: {
+          y: 300
+        },
+        debug: false
+      }
+    },
     scene: {
       preload: preload,
       create: create,
@@ -23,6 +32,7 @@ export default function Game(){
         ...config
       })
       setGame(game)
+      console.log(game)
     }
     initPhaser()
   }, [])
@@ -30,19 +40,102 @@ export default function Game(){
 
 
   function preload (){
-    this.load.image('sky', '/sky.png')
+    this.load.image('sky', '/assets/bg/sky.png')
+    this.load.image('star', '/assets/items/star.png')
+    this.load.image('ground', '/assets/items/ground.png')
+    this.load.spritesheet('player', 
+      '/assets/character/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+      
+      )
   }
+
+  let platforms
+  let player
+  let stars
 
   function create (){
     this.add.image(400, 300, 'sky')
+    // this.add.image(400, 300, 'star')
+
+    platforms = this.physics.add.staticGroup()
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+
+    platforms.create(600, 400, 'ground')
+    platforms.create(50, 250, 'ground')
+    platforms.create(750, 220, 'ground')
+
+    player = this.physics.add.sprite(100, 450, 'player')
+
+    player.setBounce(0.2)
+    player.setCollideWorldBounds(true)
+  
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3}),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'player', frame: 4 } ],
+      frameRate: 20
+    })
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    
+    stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    })
+    
+    stars.children.iterate(function (child) {
+      
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+      
+    })
+
+
+    this.physics.add.collider(player, platforms)
+    this.physics.add.collider(stars, platforms)
+    this.physics.add.overlap(player, stars, collectStar, null, this)
+
+    function collectStar (player, star) {
+      star.disableBody(true, true);
+    }
   }
 
   function update (){
+    let cursors = this.input.keyboard.createCursorKeys()
+
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160)
+      player.anims.play('left', true)
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(160)
+      player.anims.play('right', true)
+    } else {
+      player.setVelocityX(0)
+      player.anims.play('turn')
+    } if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330)
+    }
+  
   }
 
-  console.log(game)
+  
   return (<>
-  <div id='game-content' />
+    
+    <div id='game-content' />
   
   </>)
   
