15aec491dc62c8c6c9f4b47b0568bab3e7979e9c
William <wchu41@my.bcit.ca>
2023-02-13 23:40:52 -0800

    select dog page hard coded

--- src/components/SelectDog/SelectDog.jsx
+++ src/components/SelectDog/SelectDog.jsx
@@ -4,31 +4,42 @@ import Button from '../Button/Button';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
+const DogCardContainers = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    width: 80%;
+    gap: 0;
+    @media (max-width: 1000px) {
+        flex-direction: column;
+        gap: 30px;
+`
 const DogCard = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    // justify-content: center;
     align-items: center;
-    width: 250px;
-    height: 450px;
+    max-width: 250px;
+    max-height: 450px;
     background-color: white;
     border-radius: 10px;
     margin: auto;
-    padding: 40px;
+    padding: 40px 25px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `
 const DogImage = styled(Image)`
     display: block;
-    margin-bottom: 100px;
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
 `
 const StatsContainer = styled.div`
     width: 100%;
+    padding-bottom: 20px;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
     grid-template-areas:
         "a b";    
 `
@@ -44,21 +55,22 @@ const StatUnfilled = styled.div`
 `
 const StatFilled = styled.div`
     display: flex;
-    width: 20px;   
+    width: 40px;   
     height: 15px;
-    background-color: red;
+    background-color: rgb(0, 150, 250);
     border-radius: 10px;
     padding: 0;
-    margin: 13px;
 `
 
 export default function SelectCard({
-    img = "/svgs/corgi.svg"
+    easy = "/svgs/ridgehound.svg",
+    medium = "/svgs/corgi.svg",
+    hard = "/svgs/samoyed.svg",
 }) {
     return (
         <>
             <Typography
-                text="Select a Pocket Pal Breed"
+                text="Select a Pocket Pal"
                 size="2rem"
                 color="#2F2F2F"
                 weight="600"
@@ -70,78 +82,221 @@ export default function SelectCard({
                 width="100%"
                 height="100%"
             />
-            <DogCard>
-                <Typography
-                    text="Medium"
-                    size="1.2rem"
-                    color="#2F2F2F"
-                    weight="bold"
-                    family="sans-serif"
-                    align="center"
-                    display="block"
-                    margin="0px"
-                    padding="0"
-                    width="100%"
-                    height="100%"
-                />
-                <DogImage src={img} width={350} height={350} />
-                <StatsContainer>
-                    <div>
-                        <Typography
-                            text="Bark Level:"
-                            size="1rem"
-                            color="#2F2F2F"
-                            weight="regular"
-                            family="sans-serif"
-                            align="left"
-                            display="flex"
-                            margin="10px"
-                            padding="0"
-                            width="100%"
-                            height="20px"
-                        />
-                        <Typography
-                            text="Social:"
-                            size="1rem"
-                            color="#2F2F2F"
-                            weight="regular"
-                            family="sans-serif"
-                            align="left"
-                            display="flex"
-                            margin="10px"
-                            padding="0"
-                            width="100%"
-                            height="20px"
-                        />
-                        <Typography
-                            text="Energy:"
-                            size="1rem"
-                            color="#2F2F2F"
-                            weight="regular"
-                            family="sans-serif"
-                            align="left"
-                            display="flex"
-                            margin="10px"
-                            padding="0"
-                            width="100%"
-                            height="20px"
-                        />
-                    </div>
-                    <div>
-                        <StatUnfilled>
-                            {/* <StatFilled /> */}
-                        </StatUnfilled>
-                        <StatUnfilled>
-
-                        </StatUnfilled>
-                        <StatUnfilled>
-
-                        </StatUnfilled>
-
-                    </div>
-                </StatsContainer>
-                <Button text="Select" />
-            </DogCard>
+            <DogCardContainers>
+                <DogCard>
+                    <Typography
+                        text="Easy"
+                        size="1.2rem"
+                        color="#2F2F2F"
+                        weight="bold"
+                        family="sans-serif"
+                        align="center"
+                        display="block"
+                        margin="0px"
+                        padding="0"
+                        width="100%"
+                        height="100%"
+                    />
+                    <DogImage src={easy} width={350} height={350} />
+                    <StatsContainer>
+                        <div>
+                            <Typography
+                                text="Bark Level:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                            <Typography
+                                text="Social:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                            <Typography
+                                text="Energy:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                        </div>
+                        <div>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                        </div>
+                    </StatsContainer>
+                    <Button text="Select" />
+                </DogCard>
+                <DogCard>
+                    <Typography
+                        text="Medium"
+                        size="1.2rem"
+                        color="#2F2F2F"
+                        weight="bold"
+                        family="sans-serif"
+                        align="center"
+                        display="block"
+                        margin="0px"
+                        padding="0"
+                        width="100%"
+                        height="100%"
+                    />
+                    <DogImage src={medium} width={350} height={350} />
+                    <StatsContainer>
+                        <div>
+                            <Typography
+                                text="Bark Level:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                            <Typography
+                                text="Social:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                            <Typography
+                                text="Energy:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                        </div>
+                        <div>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                        </div>
+                    </StatsContainer>
+                    <Button text="Select" />
+                </DogCard>
+                <DogCard>
+                    <Typography
+                        text="Hard"
+                        size="1.2rem"
+                        color="#2F2F2F"
+                        weight="bold"
+                        family="sans-serif"
+                        align="center"
+                        display="block"
+                        margin="0px"
+                        padding="0"
+                        width="100%"
+                        height="100%"
+                    />
+                    <DogImage src={hard} width={350} height={350} />
+                    <StatsContainer>
+                        <div>
+                            <Typography
+                                text="Bark Level:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                            <Typography
+                                text="Social:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                            <Typography
+                                text="Energy:"
+                                size="1rem"
+                                color="#2F2F2F"
+                                weight="regular"
+                                family="sans-serif"
+                                align="left"
+                                display="flex"
+                                margin="10px"
+                                padding="0"
+                                width="100%"
+                                height="20px"
+                            />
+                        </div>
+                        <div>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                            <StatUnfilled>
+                                <StatFilled />
+                            </StatUnfilled>
+                        </div>
+                    </StatsContainer>
+                    <Button text="Select" />
+                </DogCard>
+            </DogCardContainers>
         </>
     )
 }
