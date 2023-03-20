2c1428aba896be97d5404f15219ea9319675797f
sn00ze <cjavillo@my.bcit.ca>
2023-03-19 17:08:58 -0700

    new instance of phaser class scene add

--- src/components/Game/game.jsx
+++ src/components/Game/game.jsx
@@ -6,6 +6,173 @@ export default function Game(){
 
   const [game, setGame] = useState({})
 
+
+  let platforms
+  let player
+  let stars
+  let score = 0
+  let scoreText
+  let bowlText
+  let hunger = 100
+
+
+  let sceneA = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize: function sceneA(){
+      Phaser.Scene.call(this, {key: 'sceneA'})
+    },
+
+    preload: function(){
+      this.load.image('sky', '/assets/bg/sky.png')
+      this.load.image('star', '/assets/items/star.png')
+      this.load.image('ground', '/assets/env/floor.png')
+      this.load.image('wall', '/assets/env/wall.png')
+      this.load.image('platform', '/assets/env/platform.png')
+      this.load.image('door', 'assets/env/door.png')
+      this.load.image('sponge', 'assets/items/sponge.png')
+      this.load.image('bowl', '/assets/items/bowl.png')
+      this.load.image('water', '/assets/items/water.png')
+      this.load.spritesheet('player', 
+        '/assets/character/spritesheet1.png',
+        { frameWidth: 344, frameHeight: 369 }
+        )
+    },
+
+    create: function(){
+
+
+
+      this.add.image(400, 300, 'sky')
+      // this.add.image(400, 300, 'star')
+  
+      platforms = this.physics.add.staticGroup()
+  
+      // platforms.create(419.5, 400, 'ground').setScale(1).refreshBody()
+  
+      this.add.image(400, 280, 'wall')
+      this.add.image(419.5, 280, 'ground')
+      this.add.image(150, 215, 'door')
+      platforms.create(400, 600, 'platform')
+      player = this.physics.add.sprite(150, 100, 'player').setScale(0.4).setCrop(0, 0, 400, 600)
+      let bowl = this.physics.add.image(500, 600, 'bowl')
+      let sponge = this.physics.add.image(200, 600, 'sponge')
+      let water = this.physics.add.image(300, 600, 'water')
+      
+      // platforms.create(600, 400, 'ground')
+      // platforms.create(50, 250, 'ground')
+      // platforms.create(750, 220, 'ground')
+      
+      this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1
+      })
+  
+      this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'player', frame: 4 } ],
+        frameRate: 20
+      })
+  
+      this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+      })
+      
+      //bounce for objects
+      player.setBounce(0.2)
+      player.setCollideWorldBounds(true)
+      sponge.setCollideWorldBounds(true)
+      water.setCollideWorldBounds(true)
+      bowl.setCollideWorldBounds(true)
+      sponge.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+      sponge.setVelocity(100, 100)
+  
+  
+      //events
+      this.input.setDraggable(sponge.setInteractive())
+      this.input.setDraggable(bowl.setInteractive())
+      this.input.setDraggable(water.setInteractive())
+      this.input.on('dragstart', function (pointer, obj){
+          obj.body.moves = false;
+      })
+  
+      this.input.on('drag', function (pointer, obj, dragX, dragY){
+          obj.setPosition(dragX, dragY);
+      });
+  
+      this.input.on('dragend', function (pointer, obj){
+          obj.body.moves = true;
+      })
+      
+  
+      
+      stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+      })
+      
+      stars.children.iterate(function (child) {
+        
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        
+      })
+  
+      scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' })
+      bowlText = this.add.text(16, 64, `hunger: ${hunger}`, { fontSize: '32px', fill: '#000' })
+  
+      //physics
+      this.physics.add.collider(player, platforms)
+      this.physics.add.collider(stars, platforms)
+      this.physics.add.overlap(player, stars, collectStar, null, this)
+      this.physics.add.collider(sponge, platforms)
+      this.physics.add.collider(sponge, player)
+      this.physics.add.collider(bowl, platforms)
+      this.physics.add.overlap(bowl, player, hitBowl, null, this)
+      this.physics.add.collider(water, platforms)
+      
+      function hitBowl(){
+  
+        if (hunger > 0){
+          hunger -= 1
+          bowlText.setText('hunger: ' + hunger) 
+        }
+        
+      }
+  
+      function collectStar (player, star) {
+  
+        score += 10
+        scoreText.setText('Score: ' + score)
+  
+        star.disableBody(true, true)
+      }
+    },
+
+    update: function(){
+      let cursors = this.input.keyboard.createCursorKeys()
+
+      if (cursors.left.isDown) {
+        player.setVelocityX(-160)
+        player.anims.play('left', true)
+      } else if (cursors.right.isDown) {
+        player.setVelocityX(160)
+        player.anims.play('right', true)
+      } else {
+        player.setVelocityX(0)
+        player.anims.play('turn')
+      } if (cursors.up.isDown && player.body.touching.down) {
+          player.setVelocityY(-330)
+      }
+    }
+  })
+
   let config = {
     type: Phaser.AUTO,
     width: 800,
@@ -19,11 +186,7 @@ export default function Game(){
         debug: false
       }
     },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    }
+    scene: [sceneA]
   }
 
   useEffect(() => {
@@ -36,157 +199,6 @@ export default function Game(){
     }
     initPhaser()
   }, [])
-
-
-
-  function preload (){
-    this.load.image('sky', '/assets/bg/sky.png')
-    this.load.image('star', '/assets/items/star.png')
-    this.load.image('ground', '/assets/env/floor.png')
-    this.load.image('wall', '/assets/env/wall.png')
-    this.load.image('platform', '/assets/env/platform.png')
-    this.load.image('door', 'assets/env/door.png')
-    this.load.image('sponge', 'assets/items/sponge.png')
-    this.load.image('bowl', '/assets/items/bowl.png')
-    this.load.spritesheet('player', 
-      '/assets/character/spritesheet1.png',
-      { frameWidth: 344, frameHeight: 369 }
-      
-      )
-  }
-
-  let platforms
-  let player
-  let stars
-  let score = 0
-  let scoreText
-  let bowlText
-  let hunger = 100
-
-  function create (){
-    this.add.image(400, 300, 'sky')
-    // this.add.image(400, 300, 'star')
-
-    platforms = this.physics.add.staticGroup()
-
-    // platforms.create(419.5, 400, 'ground').setScale(1).refreshBody()
-
-    this.add.image(400, 280, 'wall')
-    this.add.image(419.5, 280, 'ground')
-    this.add.image(150, 215, 'door')
-    platforms.create(400, 600, 'platform')
-    player = this.physics.add.sprite(150, 100, 'player').setScale(0.4).setCrop(0, 0, 400, 600)
-    let bowl = this.physics.add.image(500, 100, 'bowl')
-    let sponge = this.physics.add.image(200, 200, 'sponge')
-    
-    // platforms.create(600, 400, 'ground')
-    // platforms.create(50, 250, 'ground')
-    // platforms.create(750, 220, 'ground')
-    
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3}),
-      frameRate: 10,
-      repeat: -1
-    })
-
-    this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'player', frame: 4 } ],
-      frameRate: 20
-    })
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
-    })
-    
-    //bounce for objects
-    player.setBounce(0.2)
-    player.setCollideWorldBounds(true)
-    sponge.setCollideWorldBounds(true)
-    bowl.setCollideWorldBounds(true)
-    sponge.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
-    sponge.setVelocity(100, 100)
-
-
-    //events
-    this.input.setDraggable(sponge.setInteractive())
-    this.input.on('dragstart', function (pointer, obj){
-        obj.body.moves = false;
-    })
-
-    this.input.on('drag', function (pointer, obj, dragX, dragY){
-        obj.setPosition(dragX, dragY);
-    });
-
-    this.input.on('dragend', function (pointer, obj){
-        obj.body.moves = true;
-    })
-    
-
-    
-    stars = this.physics.add.group({
-      key: 'star',
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 }
-    })
-    
-    stars.children.iterate(function (child) {
-      
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-      
-    })
-
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' })
-    bowlText = this.add.text(16, 64, `hunger: ${hunger}`, { fontSize: '32px', fill: '#000' })
-
-    //physics
-    this.physics.add.collider(player, platforms)
-    this.physics.add.collider(stars, platforms)
-    this.physics.add.overlap(player, stars, collectStar, null, this)
-    this.physics.add.collider(sponge, platforms)
-    this.physics.add.collider(bowl, platforms)
-    this.physics.add.collider(bowl, player, hitBowl)
-    
-    function hitBowl(){
-
-      if (hunger > 0){
-        hunger -= 1
-        bowlText.setText('hunger: ' + hunger) 
-      }
-      
-    }
-
-    function collectStar (player, star) {
-
-      score += 10
-      scoreText.setText('Score: ' + score)
-
-      star.disableBody(true, true)
-    }
-  }
-
-  function update (){
-    let cursors = this.input.keyboard.createCursorKeys()
-
-    if (cursors.left.isDown) {
-      player.setVelocityX(-160)
-      player.anims.play('left', true)
-    } else if (cursors.right.isDown) {
-      player.setVelocityX(160)
-      player.anims.play('right', true)
-    } else {
-      player.setVelocityX(0)
-      player.anims.play('turn')
-    } if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330)
-    }
-  
-  }
-
   
   return (<>
     
