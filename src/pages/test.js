57f02f0844809abb3e46a179d365623cc9968acd
Ishan-sa <ishansach19@gmail.com>
2023-03-28 18:59:05 -0700

    fucking weather

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,83 +1,47 @@
-import Image from "next/image";
-import { Child, Container, Li, Type, Ul } from "src/styles/styles";
-import styled from "styled-components";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-const StyledImg = styled(Image)`
-  border-radius: 0.5rem;
-`;
+export default function test() {
+  const [weather, setWeather] = useState();
+  const [test, setTest] = useState("test");
+  async function getWeather() {
+    const location = "vancouver";
+    const units = "metric";
+    const apiKey = process.env.NEXT_PUBLIC_WEATHER_API;
+    const lang = "en";
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&lang=${lang}&appid=${apiKey}`
+    );
+    console.log(response.data);
+    setWeather(response.data);
+  }
 
-export default function handler({ txt, description, tags, src }) {
-  const typeList = ["Type", "Status", "Size", "Breed", "Attributes"];
-  const tagList = ["Tag1", "Tag2", "Tag3", "Tag4", "Tag5"];
+  useEffect(() => {
+    getWeather();
+  }, []);
 
   return (
     <>
-      <Container paddingMobile="20px 10px">
-        <Container flexDirection="column" padding="40px 30px">
-          <Container>
-            <StyledImg
-              src={src || "/placeholders/placeholder-image.png"}
-              width={500}
-              height={500}
-              alt="Picture"
-            />
-          </Container>
-        </Container>
-        <Container
-          flexDirection="column"
-          gap="20px"
-          padding="40px 30px"
-          paddingMobile="20px 10px"
-          gapMobile="0px"
-        >
-          <div>
-            <h1 className="text-xl">Name</h1>
-          </div>
-          <Ul flexDirection="column" gap="10px">
-            {typeList.map((item, index) => {
-              return (
-                <Li key={index}>
-                  <span
-                    style={{
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {item}:
-                  </span>{" "}
-                  {txt || "Not Available"}
-                </Li>
-              );
-            })}
-          </Ul>
-          <Container>
-            <Type size="0.9rem">
-              {description ||
-                "Bite the neighbor's bratty kid purr like a car engine oh yes, there is my human slave woman she does best pats ever that all i like about her hiss meow iâ€™m so hungry iâ€™m so hungry but ew not for that . Munch, munch, chomp, chomp see owner, run in terror, i like fish so purrr purr littel cat, little cat purr purr. Then cats take over the world poop on grasses, for missing until dinner time, yet give attitude, or bite the neighbor's bratty kid find empty spot in cupboard and sleep all day, or that box? i can fit in that box. Paw at beetle and eat it before it gets away side-eyes your other hand while being petted and demand to have some of whatever the human is cooking, then sniff the offering and walk away weigh eight pounds but take up a full-size bed, and if human is on laptop sit on the keyboard for poop on the floor, break a planter, sprint, eat own hair, vomit hair, hiss"}
-            </Type>
-          </Container>
-          <Container
-            gap="15px"
-            flexDirectionMobile="row"
-            justifyContentMobile="flex-start"
-            alignItemsMobile="flex-start"
-            width="100%"
-            gapMobile="10px"
-            marginMobile="1rem"
-          >
-            {tagList.map((o, i) => {
-              return (
-                <>
-                  <Child key={i}>
-                    <Type size="0.8rem">{tags || o}</Type>
-                  </Child>
-                </>
-              );
-            })}
-          </Container>
-        </Container>
-      </Container>
+      <h1>Test</h1>
+      <h2>{weather?.name}</h2>
+      <h3>{weather?.main.temp}</h3>
+      <h3>{weather?.weather[0].description}</h3>
     </>
   );
 }
+
+// export async function getServerSideProps() {
+//   const location = "vancouver";
+//   const units = "metric";
+//   const apiKey = process.env.NEXT_PUBLIC_WEATHER_API;
+//   const lang = "en";
+//   const response = await axios.get(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&lang=${lang}&appid=${apiKey}`
+//   );
+//   console.log(response.data);
+//   return {
+//     props: {
+//       data: response.data,
+//     },
+//   };
+// }
